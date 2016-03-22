576c20a26b3014a4eda91f23853b5856af390dba
Bonnie Bright <blbright@swbell.net>
2016-03-22 15:20:12 -0500

    some other shiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :comments
-  resources :groups
-  resources :posts
   devise_for :users
+  resources :groups do
+    resources :posts do
+      resources :comments
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
