2725cdeca9dffeb38305278bee492c25f4283572
Christy Leung <christy@leafmedium.com>
2016-03-23 13:36:47 -0500

    dot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,11 +18,25 @@ class PostsController < ApplicationController
   @post = Post.find(params[:id])
  end
 
- def destroy
+def destroy
   @post = Post.find(params[:id])
-  @post.destroy
-  redirect_to '/'
- end
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+end
 
  private
 
