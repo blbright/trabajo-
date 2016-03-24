ea39dfed5107cf7bd8377f9d0fe1d8d0e9c0b36f
zolon wilkins <zolonsk8@gmail.com>
2016-03-23 20:25:00 -0500

    s3 image hosting

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'pry'
 
 gem 'paperclip'
 gem 'groupify'
-
+gem 'aws-sdk'
 gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 groupify :group_member
 groupify :named_group_member
 
-has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", square: '200x200#' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
  has_many :posts, dependent: :destroy
