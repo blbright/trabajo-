f20ebbaa9c238d32556d46b4520229dfe5d1f5f6
zolon wilkins <zolonsk8@gmail.com>
2016-03-25 16:00:46 -0500

    fixed user mailer folder

--- app/controllers/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-    default :from => "trabadoresapp@gmail.com"
-  ADD EMAIL ADDRESS
-
- def registration_confirmation(user)
-    @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
- end
-end
