1574ad34fcd9506436dc889b47064f8847be675f
zolon wilkins <zolonsk8@gmail.com>
2016-03-25 11:06:32 -0500

    fixed user delete bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes
   has_many :vote_users, through: :vote, source: :user
 
